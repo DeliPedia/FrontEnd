544ca233dc8c20fdf2051f8c8629fec81c90e2dc
htwenty-1 <h2ne1.public@gmail.com>
2022-06-04 21:49:39 +0900

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import RootRouter from "~/pages"
+import RootRouter from "./pages"
 import { BrowserRouter } from "react-router-dom"
 import "~/i18n"
-import theme from "~/styles/themes"
+import theme from "./styles/themes"
 import { ThemeProvider } from "styled-components"
-import GlobalStyle from "~/styles/globalStyles"
+import GlobalStyle from "./styles/globalStyles"
 import { RecoilRoot } from "recoil"
 
 function App() {
